ae6b44cdd3aa136193ed46fc6f9088ce37d2b1c1
Kus Cámara <kcmr@users.noreply.github.com>
2017-10-14 14:17:52 +0200

    refactor(bin): change default command

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,23 @@
 #! /usr/bin/env node
 
-const cssapi = require('../lib/cssapi');
+const yargs = require('yargs');
+const component = require('path').basename(process.cwd());
 
-cssapi.updateCSSDocs(require('path').basename(process.cwd()));
+const commands = {
+  'cssapi': () => require('../lib/cssapi').updateCSSDocs(component),
+  'public-api': () => require('../lib/apidocs').writeApi(component)
+};
+
+const argv = yargs
+  .command('cssapi', 'Prints a markdown table with the CSS API of the component')
+  .command('public-api', 'Writes a file (public-api.json) with the public API of the component')
+  .demandCommand()
+  .help()
+  .argv;
+
+if (commands[argv._[0]]) {
+  commands[argv._[0]]();
+} else {
+  console.log('Unknown command. Run "most help" for a list of available commands.');
+}
+
